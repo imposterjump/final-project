d2c501db209577570343fb11ed9af577cbc43abe
imposterjump <125587220+imposterjump@users.noreply.github.com>
2023-05-15 19:58:44 +0200

    working home page but java script only

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 var router = Router();
 
 router.get('/', function(req, res, next) {
-    res.send('i am here for you ya me3alem akalemak men el home page ');
+    res.render('home');
 });
 
 export default router;
