10bedfc50a9301b69b8f5026d3850b06deb4e766
imposterjump <125587220+imposterjump@users.noreply.github.com>
2023-05-27 10:29:00 +0200

    finally working search in admin  users management

--- routes/admin-user-management.js
+++ routes/admin-user-management.js
@@ -16,5 +16,38 @@ router.get('/', function(req, res, next) {
 
 
 });
+router.post('/search', function(req, res, next) {
+
+    console.log(req.body.search_bar_users);
+    if (req.body.search_bar_users == "" || req.body.search_bar_users == null) {
+        users.find()
+            .then(result => {
+                console.log(result);
+                res.render('admin_user_controls', { users: result });
+                // res.render('viewAll', { employees: result, user: (req.session.user === undefined ? "" : req.session.user) });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+    } else {
+        var query = { username: req.body.search_bar_users };
+        users.find(query)
+            .then(result => {
+                console.log(result);
+                res.render('admin_user_controls', { users: result });
+                // res.render('viewAll', { employees: result, user: (req.session.user === undefined ? "" : req.session.user) });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+    }
+
+
+
+
+
+});
 
 export default router;
