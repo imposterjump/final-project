83c9d20cfc95dba7332af4b4795e7770c9d0c8ed
Omarbakary18 <125599045+Omarbakary18@users.noreply.github.com>
2023-05-31 14:10:53 +0200

    Added new price before and after for discounts

--- models/products.js
+++ models/products.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;n
 const products_schema = new Schema({
     itemName: { type: String, required: true },
     itemID: { type: String  , required: true },
     Sales: { type: Number,required: true },
     description: { type: String,required: true },
-    price: { type: Number, required: true },
+    price_before: { type: Number, required: true },
+    price_after: { type: Number, required:true},
     type: { type: String , required:true},
     images: [{ type: String,required: true }]
 }, { timestamps: true });
