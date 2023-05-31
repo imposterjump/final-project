2d3b5cf350e7319657a0ebd33428b9e974d682ea
imposterjump <125587220+imposterjump@users.noreply.github.com>
2023-05-31 23:03:29 +0200

    signup with validations and hashing working

--- routes/home.js
+++ routes/home.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import bcrypt from "bcrypt";
 var router = Router();
 
 router.get('/', function(req, res, next) {
-    let mypassword = bcrypt.hashSync('123456', SALT_ROUNDS)
 
-    let checkpass = '123456';
-    if (mypassword == checkpass) {
-        console.log("password is correct ");
-    } else {
-        console.log("password is incorrect ");
-    }
 
     res.render('HomePage');
 });
