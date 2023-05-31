605dac2a627fc9e3ea7f5a4e11aa20c3b0742c58
imposterjump <125587220+imposterjump@users.noreply.github.com>
2023-05-31 20:54:18 +0200

    packages and tryinh hashing 1

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
+import bcrypt from "bcrypt";
 var router = Router();
 
 router.get('/', function(req, res, next) {
+    let mypassword = bcrypt.hashSync('123456', SALT_ROUNDS)
+
+    let checkpass = '123456';
+    if (mypassword == checkpass) {
+        console.log("password is correct ");
+    } else {
+        console.log("password is incorrect ");
+    }
+
     res.render('HomePage');
 });
 
