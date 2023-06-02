097a47ed0eee3fe3b070fed65484b40eb2b88a6c
imposterjump <125587220+imposterjump@users.noreply.github.com>
2023-06-02 19:29:02 +0200

    working edit in admin page but still need sm work

--- routes/admin-user-management.js
+++ routes/admin-user-management.js
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 var router = Router();
+import bcrypt from "bcrypt";
+import pkg from 'url/util.js';
+const { isNullOrUndefined } = pkg;
+
 import users from "../models/users.js"
 
 router.get('/', function(req, res, next) {
@@ -44,6 +48,89 @@ router.post('/search', function(req, res, next) {
 
     }
 
+
+
+
+
+});
+router.post("/:id", function(req, res, next) {
+
+
+
+    const default_type = "user";
+    let counter = "a";
+
+
+
+    console.log("infos : " + " username:" +
+        req.body.username + " pass:" + req.body.pw + " conf pass:" + req.body.cpw + " email:" + req.body.email + " phone:" + req.body.phone);
+
+    if (req.body.username == "" || req.body.pw == "" || req.body.cpw == "" || +req.body.email == "" || req.body.phone == "") {
+        counter = false;
+        res.render('signup', {
+            TITLE: 'SIGNUP PAGE',
+            message: 'please you have to fill all the information '
+
+        });
+
+    } else if (req.body.pw != req.body.cpw) {
+        counter = "b";
+        res.render('signup', {
+            TITLE: 'SIGNUP PAGE',
+            message: 'the passwords doesnt match pls try again '
+
+        });
+
+    } else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email) == false) {
+        counter = "b";
+        res.render('signup', {
+            TITLE: 'SIGNUP PAGE',
+            message: 'the email is invalid please try again '
+
+        });
+
+
+    } else if (/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/.test(req.body.phone) == false) {
+        counter = "b";
+        res.render('signup', {
+            TITLE: 'SIGNUP PAGE',
+            message: 'the phone number is invalid pls try again  '
+
+        });
+    } else {
+
+
+
+        const SALT_ROUNDS = 10;
+
+        users.findByIdAndUpdate(req.params.id, {
+
+                username: req.body.username,
+                password: bcrypt.hashSync(req.body.pw, SALT_ROUNDS),
+
+                type: default_type,
+                email: req.body.email,
+                phone_number: req.body.phone
+            })
+            .then(result => {
+                res.redirect('/admin-user-management');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 
 
 
