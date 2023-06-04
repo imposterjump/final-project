6f07acba870bc3ba102e34ed887f2e70a4d9ec7a
Abdelrahman27996 <125595297+Abdelrahman27996@users.noreply.github.com>
2023-06-04 04:55:54 +0300

    srvr

--- routes/admin-homepage.js
+++ routes/admin-homepage.js
@@ -2,13 +2,14 @@ import { Router } from 'express';
 var router = Router();
 
 router.get('/', function(req, res, next) {
-    const analyticsData = {
+    const analyticsdata = {
         numberOforderschartdata: [10, 20, 30, 40, 50],
         numberofvisitorschartdata: [5, 10, 15, 20, 25],
         numberoforders: 550,
         numberofvisitorstoday: 600,
         registeredusers: 6500,
-        tobefulfilled:26
+        tobefulfilled:26,
+        totalsales:4947,
 
 
     };
