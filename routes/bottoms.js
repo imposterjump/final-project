9fc25c681f0c06f156b551da7caec21d42fe9bcc
Omarbakary18 <125599045+Omarbakary18@users.noreply.github.com>
2023-06-04 18:43:35 +0200

    changed bottoms routes to use database

--- routes/bottoms.js
+++ routes/bottoms.js
@@ -2,9 +2,25 @@ import { Router } from 'express';
 var router = Router();
 
 router.get('/', function(req, res, next) {
-    res.render('Bottoms', {
-        user: (req.session.user === undefined ? "" : req.session.user)
-    });
-});
 
+    Product.find({ type: 'jackets' })
+        .then(result => {
+            console.log(result);
+            res.render('bottoms', {
+                Product: result,
+                TITLE: 'PRODUCT PAGE',
+                message: '',
+
+                user: (req.session.user === undefined ? "" : req.session.user)
+
+
+
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+
+});
 export default router;
