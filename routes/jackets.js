50bacffae3be37f7aed6f2e67e02d6c986d567b5
Omarbakary18 <125599045+Omarbakary18@users.noreply.github.com>
2023-06-04 14:38:00 +0200

    trying to solve the problem

--- routes/jackets.js
+++ routes/jackets.js
@@ -1,6 +1,9 @@
-import { Router } from 'express';
-import Product from "../models/Product.js";
-var router = Router();
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { createRequire } from 'module';
+import Product from '../models/Product.js';
+const router = express.Router();
 
 router.get('/', function(req, res, next) {
     res.render('jackets', {
@@ -8,4 +11,16 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.get('/', (req, res) => {
+    const productId = req.params.id;
+
+    Product.findById(productId)
+        .then((product) => {
+            res.render('details', { product });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.redirect('/');
+        });
+});
 export default router;
