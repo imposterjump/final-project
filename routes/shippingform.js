ec20700306ec9dbc1ff4999411d26dc11f60d10e
OmarAbdelmonem1 <omar2108230@miuegypt.edu.eg>
2023-06-06 09:47:29 +0300

    order-item working in db

--- routes/shippingform.js
+++ routes/shippingform.js
@@ -6,15 +6,42 @@ import OrderItem from '../models/order-item.js'
 
 const router = Router();
 //get shipping form
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  const { productId, quantity } = req.body;
 
+  let orderItems;
 
+  if (Array.isArray(productId) && Array.isArray(quantity)) {
+    // Assuming productId and quantity are arrays
+    orderItems = productId.map((id, index) => ({
+      productId: id,
+      quantity: quantity[index],
+    }));
+  } else {
+    // Assuming productId and quantity are single values
+    orderItems = [{
+      productId,
+      quantity,
+    }];
+  }
 
-    res.render('shippingform', {
-        user: (req.session.user === undefined ? "" : req.session.user)
+  try {
+    // Save order items to the database
+    const savedOrderItems = await OrderItem.create(orderItems);
+
+    // Console.log all products with their quantities
+    
+    savedOrderItems.forEach((item) => {
+      console.log(`Product ID: ${item.productId}, Quantity: ${item.quantity}`);
     });
 
-
-
+    // Return the saved order items in the response
+    res.json(savedOrderItems);
+  } catch (error) {
+    console.error(error);
+    // Handle the error and send an appropriate response
+    res.status(500).json({ error: 'Failed to save order items to the database' });
+  }
 });
+
 export default router;
